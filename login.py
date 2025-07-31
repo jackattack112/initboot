5bebb237ddc89ce0d81314663e3ff211a5f2a41e
jackattack112 <jackattack112@proton.me>
2025-07-31 22:28:33 +0000

    Create login.py

---
+++ login.py
@@ -0,0 +1,9 @@
+import undetected_chromedriver as uc
+import time
+driver = uc.Chrome()
+driver.get("https://account.proton.me/login")
+driver.switch_to.new_window('tab')
+driver.get("https://github.com/login")
+#keep windows open forever
+while (1)
+    time.sleep(1000000000000)
