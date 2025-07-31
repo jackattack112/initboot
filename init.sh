3ad9c6519d08f55598f66a18e3152aa77ad555b5
jackattack112 <jackattack112@proton.me>
2025-07-31 22:07:14 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,15 @@
-#join tailscale
-curl -fsSL https://tailscale.com/install.sh | sh
-sudo tailscale up --accept-routes
-
 #install selenium
 python3 -m venv venv/
-source venv/bin/activate 
+source venv/bin/activate
 pip3 install selenium
+
+#install chrome
+curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.d>
+sudo apt install ./google-chrome-stable_current_amd64.deb
 
 #run selenium script
 python3 logins.py
+
+#join tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
+sudo tailscale up --accept-routes
