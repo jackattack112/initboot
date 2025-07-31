f606080bd234562827e7a99c0f16d16c1eb36471
jackattack112 <jackattack112@proton.me>
2025-07-31 22:34:37 +0000

    fixed syntax

--- login.py
+++ login.py
@@ -5,5 +5,5 @@ driver.get("https://account.proton.me/login")
 driver.switch_to.new_window('tab')
 driver.get("https://github.com/login")
 #keep windows open forever
-while (1)
+while (1):
     time.sleep(1000000000000)
