4882e37572afab1a4eb0a5ba3cb860ab9cec507c
jackattack112 <jackattack112@proton.me>
2025-07-31 23:00:03 +0000

    Update login.py

--- login.py
+++ login.py
@@ -1,9 +1,17 @@
 import undetected_chromedriver as uc
 import time
+import os
+
 driver = uc.Chrome()
-driver.get("https://account.proton.me/login")
-driver.switch_to.new_window('tab')
+##driver.get("https://account.proton.me/login")
+##input("Press any value after logging in: ")
+##driver.switch_to.new_window('tab')
 driver.get("https://github.com/login")
+input("Press any value after logging in: ")
+
+os.system('curl -fsSL https://tailscale.com/install.sh | sh')
+os.system('sudo tailscale up --accept-routes')
+
 #keep windows open forever
 while (1):
     time.sleep(999999999)
